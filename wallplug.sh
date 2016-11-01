ba3c7e8f0ca99bdce1459f4b19368da65b8828e1
Nikolai Magnussen <nikolai@magnussen.tf>
2016-11-01 17:56:31 +0100

    Jeg er dum i hoden min.

--- wallplug.sh
+++ wallplug.sh
@@ -5,8 +5,8 @@
 
 # Turn power on
 while true; do
-curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_4-0-3$
+curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_4-0-3/command/on
 sleep 1
-curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_4-0-3$
+curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_4-0-3/command/off
 sleep 1
 done
