0d42fd2a6fe6cf20fb196b65d7612d16f9cd5886
Nikolai Magnussen <nikolai@magnussen.tf>
2016-11-01 17:45:15 +0100

    Tried to turn light on

--- wallplug.sh
+++ wallplug.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Get devices
 curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices
+
+# Turn power on
+curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/devices/ZWayVDev_zway_4-0-37/command/on
