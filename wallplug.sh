537d87f81a96d37be7f2980fd5d3c376c9d28ebe
Nikolai Magnussen <nikolai@magnussen.tf>
2016-11-01 17:53:29 +0100

    Stupid and useless loop.

--- wallplug.sh
+++ wallplug.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Get devices
-curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices
+#curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices
 
 # Turn power on
-curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/devices/ZWayVDev_zway_4-0-37/command/on
+while true; do
+curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_4-0-3$
+sleep 1
+curl http://admin:WelcometoCX01@10.0.1.14:8083/ZAutomation/api/v1/devices/ZWayVDev_zway_4-0-3$
+sleep 1
+done
